98936b3c87c69fc034438c188cd21a73237f4782
Philip Sabri <4808216+philipsabri@users.noreply.github.com>
2023-03-15 13:31:59 +0100

    Update entrypoint.sh with ${GITHUB_WORKSPACE}/${source} for adding git trusted

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,8 @@ patch_string_token=${PATCH_STRING_TOKEN:-#patch}
 none_string_token=${NONE_STRING_TOKEN:-#none}
 branch_history=${BRANCH_HISTORY:-compare}
 # since https://github.blog/2022-04-12-git-security-vulnerability-announced/ runner uses?
-git config --global --add safe.directory /github/workspace
+#git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory ${GITHUB_WORKSPACE}/${source} #philip test
 
 cd "${GITHUB_WORKSPACE}/${source}" || exit 1
 
